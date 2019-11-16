4e4338909eab05405e8d1d0b6e4f892c352bf536
BrandonBrown <brandonbrown@pursuit.org>
2019-11-16 17:10:58 -0500

    Question 2

--- recursion.js
+++ recursion.js
@@ -7,5 +7,15 @@ return num + sumAll(num-1)
 
 console.log(sumAll(5))
 
+console.log('QUESTION 2')
+const multArr = (arr, i = 0, prod = 1) => {
+    if(i >= arr.length) return prod
+    prod *= arr[i] 
+    i++
+    return multArr(arr, i, prod)
+}
+console.log(multArr([1,2,3,4]))
+
+
 
     
